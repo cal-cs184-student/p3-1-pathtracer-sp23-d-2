ad03343140e6a80d51ee430d8776f8d5e8c9a7b0
Michael Lin <miclin@berkeley.edu>
2023-03-13 11:47:00 -0700

    part 1 done

--- src/scene/sphere.cpp
+++ src/scene/sphere.cpp
@@ -15,6 +15,23 @@ bool Sphere::test(const Ray &r, double &t1, double &t2) const {
     // Return true if there are intersections and writing the
     // smaller of the two intersection times in t1 and the larger in t2.
 
+    double a = dot(r.d, r.d);
+    double b = 2 * dot(r.o - o, r.d);
+    double c = dot(r.o - o, r.o - o) - r2;
+
+    double d = b * b - 4 * a * c;
+    if (d < 0)
+        return false;
+
+    t1 = (-b + sqrt(d)) / 2 / a;
+    t2 = (-b - sqrt(d)) / 2 / a;
+
+    if (t1 > t2) {
+        d = t1; t1 = t2; t2 = d;
+    }
+
+    if (t1 < r.min_t || t2 > r.max_t)
+        return false;
 
     return true;
 
@@ -26,8 +43,8 @@ bool Sphere::has_intersection(const Ray &r) const {
     // Implement ray - sphere intersection.
     // Note that you might want to use the the Sphere::test helper here.
 
-
-    return true;
+    Intersection _;
+    return intersect(r, &_);
 }
 
 bool Sphere::intersect(const Ray &r, Intersection *i) const {
@@ -38,7 +55,20 @@ bool Sphere::intersect(const Ray &r, Intersection *i) const {
     // When an intersection takes place, the Intersection data should be updated
     // correspondingly.
 
+    double t1, t2;
+    bool rayTest = test(r, t1, t2);
 
+    if (!rayTest) {
+        return false;
+    }
+
+    i->t = t1;
+    i->n = r.o + r.d * t1 - o;
+    i->n.normalize();
+    i->primitive = this;
+    i->bsdf=get_bsdf();
+
+    r.max_t = t1;
     
     return true;
 }
